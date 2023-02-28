67d5c2ce3600ded31a8e8edabe1603672523e6c2
Karen <karencinca@hotmail.com>
2023-02-27 23:31:29 -0300

    input component

--- Stage 9/codando-junto-react/rocketnotes/src/main.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-import { Details } from './pages/Details'
+import { Home } from './pages/Home'
 import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
--- Stage 9/codando-junto-react/rocketnotes/src/pages/Details/index.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/pages/Details/index.jsx
@@ -1,7 +1,9 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 
 export function Details() {
@@ -9,14 +11,33 @@ export function Details() {
     <Container>
         <Header />
 
-        <Section title="Links úteis">
-          <Links>
-            <li><a href="#">https://rocketseat.com.br</a></li>
-            <li><a href="#">https://rocketseat.com.br</a></li>
-          </Links>
-        </Section>
+        <main>
+          <Content>
 
-        <Button title="Voltar"/>
+            <ButtonText title="Excluir a nota" />
+
+            <h1>Introdução ao React</h1>
+
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis commodi pariatur, dicta culpa neque voluptas cupiditate ratione facilis eos. Ratione suscipit non mollitia quo laboriosam eius officiis illum doloribus repudiandae! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus corporis molestiae recusandae fugiat velit impedit. Eligendi, quaerat rem minima cumque, totam dicta ratione, error molestiae vitae asperiores ipsum amet autem.
+            </p>
+
+            <Section title="Links úteis">
+              <Links>
+                <li><a href="#">https://rocketseat.com.br</a></li>
+                <li><a href="#">https://rocketseat.com.br</a></li>
+              </Links>
+            </Section>
+
+            <Section title="Marcadores">
+                <Tag title="express" />
+                <Tag title="nodejs" />
+            </Section>
+
+            <Button title="Voltar"/>
+
+        </Content>
+        </main>
     </Container>
     
   )
