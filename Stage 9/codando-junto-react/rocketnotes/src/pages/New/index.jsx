ee644f08a99ba8456af0386a671ce1dcc4478e07
Karen <karencinca@hotmail.com>
2023-03-02 23:37:26 -0300

    new item

--- Stage 9/codando-junto-react/rocketnotes/src/pages/New/index.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/pages/New/index.jsx
@@ -1,6 +1,9 @@
+import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
 import { Container, Form } from './styles'
 
 export function New() {
@@ -18,6 +21,20 @@ export function New() {
                     <Input placeholder="Título" />
                     <Textarea placeholder="Observações"/>
 
+                    <Section title="Links úteis">
+                        <NoteItem />
+                        <NoteItem isNew placeholder="Novo link" />
+                    </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                           <NoteItem value="react"/>
+                            <NoteItem isNew placeholder="Nova tag" /> 
+                        </div>
+                        
+                    </Section>
+
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
