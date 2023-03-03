44ef7523972d402c78aaa0837782b02942e82fd2
Karen <karencinca@hotmail.com>
2023-03-03 00:30:53 -0300

    routes

--- Stage 9/codando-junto-react/rocketnotes/src/main.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/main.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-import { New } from './pages/New'
+
+import { Routes } from './routes'
 import GlobalStyles from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
