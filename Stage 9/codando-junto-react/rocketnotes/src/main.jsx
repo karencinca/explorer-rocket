3ea31df282a59ff4dcc34f4798424266a519dd83
Karen <karencinca@hotmail.com>
2023-04-16 22:16:51 -0300

    redirecting routes

--- Stage 9/codando-junto-react/rocketnotes/src/main.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 
+import { AuthProvider } from './hooks/auth'
+
 import { Routes } from './routes'
 import GlobalStyles from './styles/global'
 
@@ -10,7 +12,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Routes />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
+      
     </ThemeProvider>
   </React.StrictMode>
 )
--- Stage 9/codando-junto-react/rocketnotes/src/pages/SignIn/index.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/pages/SignIn/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import { Container, Form, Background } from "./styles";
 import { Link } from 'react-router-dom'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
+import { useAuth } from "../../hooks/auth";
+
 import { FiMail, FiLock } from 'react-icons/fi'
  
 export function SignIn() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const { signIn } = useAuth()
+
+    function handleSignIn() {
+        signIn({ email, password })
+    }
+
     return (
         <Container>
             <Form>
@@ -18,15 +30,17 @@ export function SignIn() {
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <Input 
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn}/>
 
                 <Link to="/register">
                     Criar conta
