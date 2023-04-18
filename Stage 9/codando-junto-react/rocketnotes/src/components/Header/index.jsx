ddda0b9b53130a666b6fcfe1c3767f5ce578c9d7
Karen <karencinca@hotmail.com>
2023-04-17 23:19:30 -0300

    user avatar

--- Stage 9/codando-junto-react/rocketnotes/src/components/Header/index.jsx
+++ Stage 9/codando-junto-react/rocketnotes/src/components/Header/index.jsx
@@ -1,17 +1,21 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from "./styles";
 import { useAuth } from "../../hooks/auth"
+import { api } from '../../services/api'
 
 export function Header() {
-    const { signOut } = useAuth()
+    const { signOut, user } = useAuth()
+
+    const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
+
     return (
         <Container>
             <Profile to="/profile">
-                <img src="https://github.com/karencinca.png" alt="Minha foto" />
+                <img src={avatarUrl} alt={user.name} />
 
                 <div>
                     <span>Bem-vinda,</span>
-                    <strong>Karen Cinca</strong>
+                    <strong>{user.name}</strong>
                 </div>
             </Profile>
 
